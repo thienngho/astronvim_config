1d441c053e4c24d2332e24ee550243abf5dd5933
Thien Ho <thienngho@gmail.com>
2023-04-09 20:53:14 +0700

    update

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,26 +1,35 @@
--- customize mason plugins
 return {
-  -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "terraformls",
+        "tflint",
+        "ansiblels",
+        "jsonls",
+        "pyright",
+        "yamlls",
+      },
     },
   },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = {
+        "stylua",
+        "black",
+        "prettierd",
+      },
     },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      -- ensure_installed = { "python" },
-    },
+    config = function(_, opts)
+      local mason_null_ls = require "mason-null-ls"
+      local null_ls = require "null-ls"
+      mason_null_ls.setup(opts)
+      mason_null_ls.setup_handlers {
+        prettierd = function()
+          null_ls.register(null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "json", "markdown" } })
+        end,
+      }
+    end,
   },
 }
